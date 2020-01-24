80765f32d19e9c54ace9a9c11e73fd47bcfed2ad
golbanv <golbanvladislav@gmail.com>
2020-01-25 00:52:48 +0100

    Add auth functionality to comments

--- routes/comments.js
+++ routes/comments.js
@@ -35,7 +35,7 @@ router.post("/", isLoggedIn, function(req, res) {
   });
 });
 
-router.get("/:comment_id/edit", function(req, res) {
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
   Comment.findById(req.params.comment_id, function(err, foundComment) {
     if (err) {
       res.redirect("back");
@@ -48,11 +48,21 @@ router.get("/:comment_id/edit", function(req, res) {
   });
 });
 
-router.put("/:comment_id", function(req, res) {
+router.put("/:comment_id", checkCommentOwnership, function(req, res) {
   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(
     err,
     updatedComment
   ) {
+    if (err) {
+      res.redirect("back");
+    } else {
+      res.redirect("/gatherings/" + req.params.id);
+    }
+  });
+});
+
+router.delete("/:comment_id", checkCommentOwnership, function(req, res) {
+  Comment.findByIdAndRemove(req.params.comment_id, function(err) {
     if (err) {
       res.redirect("back");
     } else {
@@ -66,6 +76,25 @@ function isLoggedIn(req, res, next) {
     return next();
   }
   res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next) {
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, function(err, foundComment) {
+      if (err) {
+        console.log(err);
+        res.redirect("back");
+      } else {
+        if (foundComment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
 }
 
 module.exports = router;
